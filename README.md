188c52184ef10506e3bdc0f0bdc5954f031b8a01
kimchj <wnmh123@naver.com>
2022-02-08 00:20:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-practice
+
+node-practice
---
+++ service.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get("/hello", (req, res) => {
+
+    res.send("hello");
+});
+
+app.listen(port, () => {
+    console.log("server is running port: " + port);
+});
